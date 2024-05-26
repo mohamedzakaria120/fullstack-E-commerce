48c4a61f93e2f9d353fb61fb3145bb52002e2f32
mohamed hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2024-05-26 16:45:29 +0300

    Front end build by @omar Samir

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Registrations from "./Pages/Registrations";
+import Login from "./Pages/Login";
+import MainPageForOwner from "./Pages/MainPageForOwner";
+import MainPageForCustomer from "./Pages/MainPageForCustomer";
+import ProductDetails from "./componentsforcustomer/ProductDetails/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {" "}
+          <Route path="/" element={<Login />} />
+          <Route path="/registration" element={<Registrations />} />
+          <Route path="/mainpageforowner" element={<MainPageForOwner />} />
+          <Route path="/mainpageforuser" element={<MainPageForCustomer />} />
+          <Route
+            path="/mainpageforuser/:productId"
+            element={<ProductDetails />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
